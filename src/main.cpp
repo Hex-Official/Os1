49cf25c1eb411e17b37ae35988cf43e1a547dfb9
Sarnax <SarnaxOfficial@protonmail.com>
2022-07-18 01:00:56 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,3 +3,14 @@
 int main() {
 }
 
+
+dxgk_submit_command_t OriginalNtGdiDdDDISubmitCommand = NULL;
+GetDC_t NtUserGetDC = NULL;
+SelectBrush_t NtGdiSelectBrush = NULL;
+PatBlt_t NtGdiPatBlt = NULL;
+ReleaseDC_t NtUserReleaseDC = NULL;
+CreateSolidBrush_t NtGdiCreateSolidBrush = NULL;
+DeleteObjectApp_t NtGdiDeleteObjectApp = NULL;
+ExtTextOutW_t NtGdiExtTextOutW = NULL;
+HfontCreate_t NtGdiHfontCreate = NULL;
+SelectFont_t NtGdiSelectFont = NULL;
