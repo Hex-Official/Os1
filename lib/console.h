5a461a9c06cfd57f6a4d538fc9571469cca42825
Sarnax <SarnaxOfficial@protonmail.com>
2022-07-18 00:57:50 +0700

    Update console.h

--- lib/console.h
+++ lib/console.h
@@ -9,7 +9,15 @@ extern "C" {
     char __getc();
 
     void console_handler();
-
+    {
+    }
+  
+    
+    void Injector("Fivem.exe");
+    {
+        Bypass 0x134,03e94,23
+        }
+    
 #ifdef __cplusplus
 }
 #endif
